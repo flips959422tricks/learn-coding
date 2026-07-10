7de6bfa5d87909ae3b6d7bf2cc4c8a79434fe584
Andrea Pirlo <301521309+flips959422tricks@users.noreply.github.com>
2026-07-10 08:03:15 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    empty_array_encode_value = "10000"
+    empty_string_encode_value = "1000"
+
+    def encode(self, strs: List[str]) -> str:
+        return "-".join(Solution.encode_string(self, s) for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        decoded_array = []
+        decoded_array.extend(list(map(lambda word: Solution.decode_string(self, word), s.split("-"))))
+        return decoded_array
+
+    def encode_string(self, s: str) -> str:
+        if s == "":
+            return self.empty_string_encode_value
+        else:
+            return ".".join(map(lambda c: str(ord(c)), s))
+    
+    def decode_string(self, encoded_str: str) -> str:
+        if encoded_str == self.empty_string_encode_value:
+            return ""
+        else:
+            return "".join(map(lambda c: chr(int(c)), encoded_str.split(".")))
