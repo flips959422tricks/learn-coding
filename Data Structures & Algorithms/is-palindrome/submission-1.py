da0dfe09b183736434c9e2a49fc9597cb234a039
Andrea Pirlo <301521309+flips959422tricks@users.noreply.github.com>
2026-07-24 23:35:05 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,34 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        is_palindrome = True
+
+        lft = 0
+        rgt = len(s) - 1
+
+        while lft < rgt:
+            lft_char = s[lft].lower()
+            lft_char_valid = self.is_alphanumeric(lft_char)
+            while not lft_char_valid and lft < len(s) - 1:
+                lft = lft + 1
+                lft_char = s[lft].lower()
+                lft_char_valid = self.is_alphanumeric(lft_char)
+
+            rgt_char = s[rgt].lower()
+            rgt_char_valid = self.is_alphanumeric(rgt_char)
+            while not rgt_char_valid and rgt > 0:
+                rgt = rgt - 1
+                rgt_char = s[rgt].lower()
+                rgt_char_valid = self.is_alphanumeric(rgt_char)
+            
+            if lft < rgt and lft_char != rgt_char:
+                return False
+            
+            lft = lft + 1
+            rgt = rgt - 1
+        
+        return is_palindrome
+
+    
+    def is_alphanumeric(self, c: str) -> bool:
+        pattern = r"^[a-zA-Z0-9]$"
+        return bool(re.match(pattern, c))
